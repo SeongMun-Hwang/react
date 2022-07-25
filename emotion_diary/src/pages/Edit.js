6d798220220facf07062c04169aaa8524a0383a4
SeongMun-Hwang <hwang105814@gmail.com>
2022-07-25 22:09:24 +0900

    step 16: practice Params,useSearchParams,useNavigate

--- emotion_diary/src/pages/Edit.js
+++ emotion_diary/src/pages/Edit.js
@@ -1,8 +1,24 @@
+import {useSearchParams,useNavigate} from "react-router-dom";
+
 const Edit=()=>{
+    const navigate=useNavigate();
+    const [searchParams, setSearchParams]=useSearchParams();
+    const id=searchParams.get("id");
+    console.log(id);
+
+    const mode=searchParams.get("mode");
+    console.log(mode);
     return(
         <div>
             <h1>Edit</h1>
             <p>Here is Edit</p>
+            <button onClick={()=>setSearchParams({who:"winterlood"})}>change query string</button>
+            <button onClick={()=>{
+                navigate("/home");
+            }}>goto Home</button>
+            <button onClick={()=>{
+                navigate(-1);
+            }}>go back</button>
         </div>
     );
 };
