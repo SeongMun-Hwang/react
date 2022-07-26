6ef8f511ded02185c03441414de64c8a7d9459db
SeongMun-Hwang <hwang105814@gmail.com>
2022-07-26 21:22:36 +0900

    step 18: make basic form/style of diary

--- emotion_diary/src/App.js
+++ emotion_diary/src/App.js
@@ -5,19 +5,32 @@ import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 import RouteTest from "./components/RouteTest";
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
+                <MyHeader headtext={"app"} leftChild={<MyButton text={"왼쪽 버튼"} onClick={()=>alert("왼족 클릭")}></MyButton>}
+                    rightChild={<MyButton text={"오른쪽 버튼"} onClick={()=>alert("오른쪽 클릭")}></MyButton>}></MyHeader>
                 <h2>app.js</h2>
+
+                <MyButton text={'버튼'}
+                onClick={()=>alert("클릭")}
+                type={"positive"}/>
+                <MyButton text={'버튼'}
+                          onClick={()=>alert("클릭")}
+                          type={"negative"}/>
+                <MyButton text={'버튼'}
+                          onClick={()=>alert("클릭")}
+                          />
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                     <Route path="/new" element={<New/>}/>
                     <Route path="/edit" element={<Edit/>}/>
                     <Route path="/diary/:id" element={<Diary/>}/>
                 </Routes>
-                <RouteTest/>
             </div>
         </BrowserRouter>
     );
